517c8aa1d122b89d508b498c966062dc89405a8f
Sean Burke <seanb@eshark.net>
2011-06-24 12:53:08 -0400

    Fix to turn off transitions. Variable was renamed in 1.0b1

--- js/system.js
+++ js/system.js
@@ -1,13 +1,7 @@
-// prevent console errors on browsers without console
-if (!window.console) {
-    window.console = {};
-    window.console.log = function(){};
-}
 function loadPage(id) {
     $.mobile.changePage("#"+id,'none');
 }
 
 $(document).bind("mobileinit", function(){
-    $.mobile.defaultTransition='none';
-	$.fx.off=true;
+    $.mobile.defaultPageTransition='none';
 });
