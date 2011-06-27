36726ea5597989755b917e081e714aac46b25109
Sean Burke <seanb@eshark.net>
2011-06-27 14:23:37 -0400

    Trying a hand made system

--- js/system.js
+++ js/system.js
@@ -1,7 +1,38 @@
-function loadPage(id) {
-    $.mobile.changePage("#"+id,'none');
+var currentDiv='';
+x$(window).on('load', function(e) {
+    x$('[data-role="page"]').each(function (e, i, x) { 
+        //if(x$(e).attr('data-role')=='page') {
+            x$(e).css({visibility: 'hidden', display: 'none'});
+        //}
+    });
+    x$('#mainMenu').css({visibility: 'visible', display: 'block'});
+    currentDiv='#mainMenu';
+})
+
+function hide(div) {
+    x$(div).css({visibility: 'hidden', display: 'none'});
 }
 
-$(document).bind("mobileinit", function(){
-    $.mobile.defaultPageTransition='none';
-});
+function show(div) {
+    x$(div).css({visibility: 'visible', display: 'block'});
+}
+
+window.onpopstate=function(event) {
+    hash=location.hash;
+    console.log(hash);
+    if(empty(hash)) {
+        hash='#mainMenu';
+    }
+    hide(currentDiv);
+    show(hash);
+    currentDiv=hash;
+}
+
+empty=function(cvar) {
+    if(cvar=="" || cvar==null || cvar=="undefined") {
+        return true;
+    }
+    return false;
+}
+
+
