b79459338168dcf2f031741f82cb58e635a6f041
Sean Burke <seanb@eshark.net>
2011-12-12 22:18:52 -0500

    Changes for jQuery Mobile

--- js/system.js
+++ js/system.js
@@ -1,44 +1,10 @@
-var currentPage='';
-
-if("onhashchange" in window) {
-    window.onhashchange=function(event) {
-        changePage();
-    }
-}
-else {
-    window.setInterval(function () {
-        if(location.hash != currentPage) {
-            changePage();
-        }
-    }, 100);
-    
-}
-
-$(window).bind('load', function(e) {
-    document.addEventListener("menubutton", function() {
+$(document).ready(function() {
+   document.addEventListener("menubutton", function() {
         location.href="index.html";
     }, false);
     document.addEventListener("searchbutton", function() {
         location.href="index.html#search";        
     }, false);
-    var url=window.location.pathname;
-    var fName=url.substring(url.lastIndexOf('/')+1);
-    var dName=fName.substring(0,fName.lastIndexOf('.'));
-    console.log(dName);
-    if(dName=='index' || empty(dName)) {
-        if(empty(location.hash)) {
-            show('#mainMenu');
-            currentPage='#mainMenu';  
-        }
-    }
-    else {
-        loadImages('#'+dName);
-        show('#'+dName);
-        currentPage='#'+dName;
-    }
-    $('#searchBox').bind('click', function() {
-        $('#searchBox').val('');
-    });
     $('#searchButton').bind('click', function() {
         console.log('Searching...');
         $('#searchResults').empty();
@@ -54,26 +20,14 @@ $(window).bind('load', function(e) {
                 $('#searchResults').append(this);
             }
         });
-        if(f) {
-            loadImages('#search');
-        }
-        else {
+        if(!f) {
             $('#searchResults').append('<li>No results found...</li>');
         }
+        $('searchResults').listview("refresh");
         $('#searchResults').show();
     });
 });
 
-function hide(div) {
-    $(div).css({display: 'none'});
-}
-
-function show(div) {
-    $(div).css({display: 'block'});
-    if($(div).height()<window.innerHeight) {
-        $(div).css('height',window.innerHeight+'px');
-    }
-}
 
 $(window).bind('orientationchange', function() {
     if($(div).height()<window.innerHeight) {
@@ -86,39 +40,6 @@ empty=function(cvar) {
         return true;
     }
     return false;
-}
-
-changePage=function() {
-    hash=location.hash;
-    console.log(hash);
-    if(empty(hash)) {
-        hash='#mainMenu';
-    }
-    if($(hash).html()===null) {
-        //alert("Can't find that page!");
-        //window.history.back(); //Reset the hash to avoid any errors        
-        if(hash=='#mainMenu') {
-            location.href='index.html';
-        }
-        else {
-            location.href=hash.substring(1)+".html";
-        }
-    }
-    else {
-        loadImages(hash);
-        hide(currentPage);
-        show(hash);
-        currentPage=hash;
-    }
-}
-
-loadImages=function(hash) {
-   $(hash+' img').each(function() {
-        var name=$(this).attr('name');
-        if(!empty(name)) {
-            $(this).attr('src', name);
-        }
-    }); 
 }
 
 cleanString=function(string) {
